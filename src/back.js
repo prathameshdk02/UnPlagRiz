2e48f91a0e5b9d0659825d7f4692f9b75f5e7484
Prathamesh Dhananjay Kadve <prathameshdk02@gmail.com>
2022-09-05 23:25:51 +0530

    Designed Logo and Updated Comments.

--- src/back.js
+++ src/back.js
@@ -1,22 +1,25 @@
 const express = require('express');
 const hbs = require('hbs');
 const path = require('path');
-
 const app = express();
 
+// Defining paths for directories
 const publicPath = path.join(__dirname,'../public');
 const viewsPath = path.join(__dirname,'../templates/views');
 const partialsPath = path.join(__dirname,'../templates/partials');
 
+// Setting paths for public, views and partials...
 app.use(express.static(publicPath));
 app.set('view engine','hbs');
 app.set('views',viewsPath);
 hbs.registerPartials(partialsPath);
 
+// Handling Routes...
 app.get('/',(req,res)=>{
     res.render('main');
 })
 
+// Starting the Local Server at Port - 8000
 app.listen(8000,()=>{
     console.log("Listening on port 8000");
 });
